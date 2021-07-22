2f62d2a5e38b262c2f1ad5360ca63fe9736b7e75
Kamil <>
2021-07-22 21:37:39 +0200

    ProductItem, ProductInStock, Stock & Restaurant DTo added

---
+++ Bite_IT/Bite_IT/Models/ProductItemDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Bite_IT.Domain;
+
+namespace Bite_IT.Models
+{
+    public class CreateProductItemDto : ProductDto
+    {
+        [Required]
+        public DateTime ExpirationDate { get; private set; }
+        public int ProductInStockId { get; private set; }
+    }
+
+    public class UpdateProductItemDto : CreateProductItemDto
+    {
+        public ProductInStockDto ProductInStock { get; private set; }
+    }
+
+    public class ProductItemDto : CreateProductItemDto
+    {
+        public int Id { get; set; }
+        public ProductInStockDto ProductInStock { get; private set; }
+    }
+}
