8395bc1ed83d05c0b14bdfca202a36e15a5f6f3d
Kamil <>
2021-07-22 20:56:28 +0200

    ProductItem, ProductInStock, Restaurant, Stock added

---
+++ Bite_IT/Bite_IT/Models/Restaurant.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using Bite_IT.Models;
+
+namespace Bite_IT.Domain
+{
+    public class Restaurant
+    {
+        public int Id { get; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+
+        public IList<Employee> Employees { get; private set; }
+        
+        public IList<Stock> Stocks { get; private set; }
+
+        public IList<Order> Orders { get; private set; }
+        
+        public Menu Menu { get; private set; }
+
+        public Restaurant(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Employees = new List<Employee>();
+            Stocks = new List<Stock>();
+            Orders = new List<Order>();
+        }
+    }
+}
