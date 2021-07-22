ceb33710ff3f380c7a49bfcba01f27503b501575
Szymon <szymon0418@gmail.com>
2021-07-22 17:23:39 +0200

    Implemented Product Model

--- Bite_IT/Bite_IT/Models/Product.cs
+++ Bite_IT/Bite_IT/Models/Product.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Bite_IT.Models
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public ProductName productName { get; set; }
+        public ProductType productType { get; set; }
+        public float Quantity { get; set; }
+    }
+}
