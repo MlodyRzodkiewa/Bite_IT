79f111b909a659e63d257d4f679790e86a6e2c05
WDziadon <wojciech.dziadon@gmail.com>
2021-07-22 21:05:41 +0200

    Order, menu , employee models added

--- Bite_IT/Bite_IT/Models/Order.cs
+++ Bite_IT/Bite_IT/Models/Order.cs
@@ -1,7 +1,16 @@
-﻿namespace Bite_IT.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bite_IT.Models
 {
     public class Order
     {
-        
+        [Required] public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int TableNumber { get; set; }
+        public List<Meal> Meals { get; set; }
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
     }
 }
