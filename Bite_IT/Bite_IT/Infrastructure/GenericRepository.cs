dd418275a5050ef2dd9210c2467654fab521a423
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2021-07-25 14:31:56 +0200

    Added interface IGenericRepository and class GenericRepository (Not Implemented)

---
+++ Bite_IT/Bite_IT/Infrastructure/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Bite_IT.Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(Expression<Func<T, bool>> expression = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
