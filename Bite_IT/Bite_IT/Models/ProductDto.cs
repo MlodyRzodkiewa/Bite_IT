33b8c5166df139ac434421b9b496d1de475ca9ae
Szymon <szymon0418@gmail.com>
2021-07-25 21:46:28 +0200

    Add required for  fields

--- Bite_IT/Bite_IT/Models/ProductDto.cs
+++ Bite_IT/Bite_IT/Models/ProductDto.cs
@@ -1,6 +1,7 @@
 ﻿using Bite_IT.Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,17 @@ namespace Bite_IT.Models
 {
     public class ProductDto
     {
+        [Required]
         public int Id { get; set; }
 
     }
     public class CreateProductDto : ProductDto
     {
+        [Required]
         public ProductName productName { get; set; }
+        [Required]
         public ProductType productType { get; set; }
+        [Required]
         public float Quantity { get; set; }
     }
 }
