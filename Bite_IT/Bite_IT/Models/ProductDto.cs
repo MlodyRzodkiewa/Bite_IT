96cce16c8d064d353d94c068ae4fe2575ad7b2d4
Szymon <szymon0418@gmail.com>
2021-07-25 20:06:01 +0200

    Create ProductDTO

---
+++ Bite_IT/Bite_IT/Models/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using Bite_IT.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bite_IT.Models
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+    }
+    public class CreateProductDto : ProductDto
+    {
+        public ProductName productName { get; set; }
+        public ProductType productType { get; set; }
+        public float Quantity { get; set; }
+    }
+}
