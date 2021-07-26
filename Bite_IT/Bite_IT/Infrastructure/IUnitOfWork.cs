4964154f09111eedb3b05ad0ce318143daa270a5
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2021-07-26 17:18:10 +0200

    added and implemented IUnitOfWork and UnitOfWork files

---
+++ Bite_IT/Bite_IT/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bite_IT.Domain;
+
+namespace Bite_IT.Infrastructure
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Employee> Employees { get; }
+        IGenericRepository<Ingredient> Ingredients { get; }
+        IGenericRepository<Meal> Meals { get; }
+        IGenericRepository<Menu> Menu { get; }
+        IGenericRepository<Order> Orders { get; }
+        IGenericRepository<ProductInStock> ProductsInStock { get; }
+        IGenericRepository<ProductItem> ProductItems { get; }
+        IGenericRepository<Restaurant> Restaurants { get; }
+        IGenericRepository<Stock> Stock { get; }
+        Task Save();
+    }
+}
---
+++ Bite_IT/Bite_IT/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bite_IT.Data;
+using Bite_IT.Domain;
+
+namespace Bite_IT.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RestaurantDbContext _context;
+
+        public UnitOfWork(RestaurantDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IGenericRepository<Employee> Employees => new GenericRepository<Employee>(_context);
+        public IGenericRepository<Ingredient> Ingredients => new GenericRepository<Ingredient>(_context);
+        public IGenericRepository<Meal> Meals => new GenericRepository<Meal>(_context);
+        public IGenericRepository<Menu> Menu => new GenericRepository<Menu>(_context);
+        public IGenericRepository<Order> Orders => new GenericRepository<Order>(_context);
+        public IGenericRepository<ProductInStock> ProductsInStock => new GenericRepository<ProductInStock>(_context);
+        public IGenericRepository<ProductItem> ProductItems => new GenericRepository<ProductItem>(_context);
+        public IGenericRepository<Restaurant> Restaurants => new GenericRepository<Restaurant>(_context);
+        public IGenericRepository<Stock> Stock => new GenericRepository<Stock>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(true);
+        }
+        
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
