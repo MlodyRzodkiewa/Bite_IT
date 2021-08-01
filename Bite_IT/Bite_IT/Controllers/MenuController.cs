e0b7246a55b6a9a5d9af8802eca57f822a290f7a
Kamil <>
2021-08-01 19:05:41 +0200

    Menu controller supplemented and improved

--- Bite_IT/Bite_IT/Controllers/MenuController.cs
+++ Bite_IT/Bite_IT/Controllers/MenuController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Bite_IT.Domain;
 using Bite_IT.Infrastructure;
 using Bite_IT.Models;
 using Microsoft.AspNetCore.Http;
@@ -28,13 +30,24 @@ namespace Bite_IT.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<IList<MenuDto>>> GetMeals()
+        public async Task<ActionResult<MenuDto>> GetMenu()
         {
             try
             {
-                var menu = await _uow.Menu.GetAll();
-                var result = _mapper.Map<IList<MenuDto>>(menu);
+                var menu = await _uow.Menu.Get(m => m.RestaurantId == 1);
 
+                if (menu == null) return NotFound($"Menu for the restaurant has not been found.");
+                
+                menu.Meals = (List<Meal>) await _uow.Meals.GetAll(meal => meal.MenuId == menu.Id);
+                
+                foreach (var meal in menu.Meals)
+                {
+                    meal.Ingredients = await _uow.Ingredients.GetAll(
+                        ingredient => meal.Ingredients.Contains(ingredient));
+                }
+
+                var result = _mapper.Map<MenuDto>(menu);
+        
                 return Ok(result);
             }
             catch (Exception e)
